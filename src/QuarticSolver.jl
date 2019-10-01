4fbe3a5a43617ce4d99e71d28d22b2d8abf0af99
Forrest Gasdia <EP-Guy@users.noreply.github.com>
2019-09-30 22:48:24 -0600

    Delete some old functions

--- src/QuarticSolver.jl
+++ src/QuarticSolver.jl
@@ -6,63 +6,6 @@ ax⁴ + bx³ + cx² + dx + e = 0
 """
 module QuarticSolver
 
-using StaticArrays
-
-
-"""
-General closed-form solution of the quartic.
-
-```math
-ax⁴ + bx³ + cx² + dx + e = 0
-```
-
-Based on the ``general formula`` from https://en.wikipedia.org/wiki/Quartic_function
-"""
-function closed(a, b, c, d, e)
-
-    a² = a^2
-    b² = b^2
-
-    p = (8*a*c - 3*b²)/(8*a²)
-    q = (b^3 - 4*a*b*c + 8*a²*d)/(8*a^3)
-
-    Δ₀ = c^2 - 3*b*d + 12*a*e
-    Δ₁ = 2*c^3 - 9*b*c*d + 27*b²*e + 27*a*d^2 - 72*a*c*e
-
-    Qroots = complexcbrts((Δ₁ + sqrt(Δ₁^2 - 4*Δ₀^3))/2)
-    Qidx = 1  # try any of the roots
-
-    Q = Qroots[Qidx]
-
-
-    S = sqrt(-2/3*p + 1/(3*a)*(Q + Δ₀/Q))/2
-    while isapprox(S, zero(S), atol=1e-9)
-        # If S ≈ 0, choose a different cube root of Q
-        Qidx += 1
-        Q = Qroots[Qidx]
-        S = sqrt(-2/3*p + 1/(3*a)*(Q + Δ₀/Q))/2
-    end
-
-    S² = S^2
-
-    tmp = -b/(4*a)
-    sqrt12 = sqrt(-4*S² - 2*p + q/S)/2
-    sqrt34 = sqrt(-4*S² - 2*p - q/S)/2
-
-    x1 = -tmp - S + sqrt12
-    x2 = -tmp - S - sqrt12
-    x3 = -tmp + S + sqrt34
-    x3 = -tmp + S - sqrt34
-
-    return x1, x2, x3, x4
-end
-
-function solvedepressedquartic(a, b, c, d)
-    a3 = -b
-    b3 = a*c - 4*d
-    c3 = -a^2*d - c^2 + 4*b*d
-end
-
 """
 Return a cube roots of a complex number `z`.
 
@@ -80,49 +23,6 @@ function complexcbrts(z::Complex)
 
     return r1, r2, r3
 end
-
-"""
-See https://www.mathemania.com/lesson/cardanos-formula-solving-cubic-equations/
-"""
-function solvecubic(a₃, a₂, a₁, a₀)
-    # Depress
-    a = a₂/a₃
-    b = a₁/a₃
-    c = a₀/a₃
-
-    third = 1/3
-    thirda = a*third
-    sqrt3 = sqrt(3)
-
-    p = b - a^2/3
-    q = 2*a^3/27 - a*b/3 + c
-    Δ = (q/2)^2 + (p/3)^3
-
-    # Unit cube roots (in addition to `1`)
-    ε₁ = -1/2 - im*sqrt3/2
-    ε₂ = -1/2 + im*sqrt3/2
-
-    if real(Δ) >= 0
-        # 1 real, 2 complex roots
-        v = cbrt(-q/2 + sqrt(Δ))
-        w = -p/(3v)
-        x1 = v - w
-        x2 = v*ε₁ + w*ε₂
-        x3 = v*ε₂ + w*ε₁
-        return x1, x2, x3
-    else  # Δ < 0
-        # Complex roots
-        r = abs(-q/2 + im*sqrt(-Δ))
-        cbrtr = cbrt(r)
-        ϕ = atan(-2*sqrt(-Δ)/q)
-        ϕ < 0 && (ϕ = -ϕ)
-        x1 = 2*cbrtr*cos(ϕ/3)
-        x2 = 2*cbrtr*cos((ϕ+2π)/3)
-        x3 = 2*cbrtr*cos((ϕ+4π)/3)
-        return x1, x2, x3
-    end
-end
-
 
 function solvequadratic(a0, b0, c0)
     inva0 = 1/a0
@@ -228,11 +128,7 @@ function bookerquartic(B4, B3, B2, B1, B0)
     p = (-g + tmpsqrt)/2
     p = ifelse(abs(p) > 1e-10, p, (-g - tmpsqrt)/2)
 
-    cbrtr = cbrt(abs(p))
-    thirdθ = angle(p)/3
-    s1 = cbrtr*cis(thirdθ)
-    s2 = cbrtr*cis(2π/3+thirdθ)
-    s3 = cbrtr*cis(2π*2/3+thirdθ)
+    s1, s2, s3 = complexcbrts(p)
 
     r1 = sqrt(s1 - h/s1 - H)
     r2 = sqrt(s2 - h/s2 - H)
@@ -246,6 +142,5 @@ function bookerquartic(B4, B3, B2, B1, B0)
 
     return q1, q2, q3, q4
 end
-
 
 end # module
